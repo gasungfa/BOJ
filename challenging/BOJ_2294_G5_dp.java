94409b59332e0564212c4655e6936472e0e9a37a
gasungfa <gasungfa@gmail.com>
2025-01-23 20:18:33 +0900

    2025-01-23

---
+++ challenging/BOJ_2294_G5_dp.java
@@ -0,0 +1,35 @@
+package challenging;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2294_G5_dp {
+    static int n,k;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[n+1];
+
+        for(int i = 1;i<=n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        solve(arr);
+    }
+    static void solve(int[] arr) {
+        int[] dp = new int[k+1];
+        for(int i = 1;i<=n;i++){
+            dp[i] = Integer.MAX_VALUE-1;
+        }
+        for(int i = 1;i<=n;i++){
+            for(int j = arr[i];j<=k;j++){
+                dp[j] = Math.min(dp[j], dp[j-arr[i]]+1);
+            }
+        }
+    }
+}
